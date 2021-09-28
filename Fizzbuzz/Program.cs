ce2988b3f4f1801ad8db149013727b65c50153e0
besxrr <91481372+besxrr@users.noreply.github.com>
2021-09-28 14:34:32 +0100

    Dump

--- Fizzbuzz/Program.cs
+++ Fizzbuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Fizzbuzz
 {
@@ -6,7 +7,56 @@ namespace Fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the maximum number: ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            List<string> print = new List<string>();
+            
+            
+            
+            for (int i = 1; i <= limit; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    print.Add("Fizz");
+                }
+
+                if (i % 13 == 0)
+                {
+                    print.Add("Fezz");
+                }
+
+                if (i % 5 == 0)
+                {
+                    print.Add("Buzz");
+                }
+
+                if (i % 7 == 0)
+                {
+                    print.Add("Bang");
+                }
+
+                if (i % 11 == 0)
+                {
+                    print.Clear();
+                    print.Add("Bong");
+                    continue;
+                }
+
+                if (i % 17 == 0)
+                {
+                    print.Reverse();
+                }
+
+                if (print.Count == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(String.Join("", print));
+                    print.Clear();
+                }
+            }
         }
     }
 }
